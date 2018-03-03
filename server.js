61e3869ccdc088ab86661e1dd6470fda6808205e
KoustavHazra <36538480+KoustavHazra@users.noreply.github.com>
2018-03-03 16:50:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+var config = {
+    user:'hazrakoustav12',
+    database:'hazrakoustav12',
+    host:'db.imad.hasura.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool=new pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err) {
+            res.status(500).send(err.toString());
+        }else {
+            res.send(JSON.stringfy(result));
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
